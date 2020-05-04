909bb384b3d79808711ae76079ba8c9ffa99308a
ZachyDev <mosetizachary001@gmail.com>
2020-05-04 17:28:59 +0300

    objactMethods

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let user = {
-    name: 'moseti',
+    name: "moseti",
     age: 20,
-    gender: 'male',
-    showInfo: function(realAge){
-        console.log(`${ this.name } ${ realAge } ${ this.gender }`)
+    gender: "male",
+    showInfo: (realAge) => {
+        console.log(`${ this.name } ${ realAge } ${ this.gender }`);
     }
 }
-user.showInfo(25)
-console.log(typeof user.showInfo)
+user.showInfo(25);
+// console.log(typeof user.showInfo)
